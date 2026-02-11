27d12551768119c0789e27ffeee87f990147d721
מור קורנגולד <mork22222@gmail.com>
2026-02-11 12:42:45 +0000

    File changes

--- functions/cloudinaryEdit.ts
+++ functions/cloudinaryEdit.ts
@@ -29,6 +29,7 @@ Deno.serve(async (req) => {
 
     const publicId = uploadResult.public_id;
     let transformations = [];
+    let outputFormat = 'mp4';
 
     switch (action) {
       case 'trim': {
