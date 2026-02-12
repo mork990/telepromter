600c80558eae847c72281bd14258f2a79fd06df4
מור קורנגולד <mork22222@gmail.com>
2026-02-12 19:28:51 +0000

    File changes

--- functions/fixEyeContact.ts
+++ functions/fixEyeContact.ts
@@ -1,6 +1,7 @@
 import { createClientFromRequest } from 'npm:@base44/sdk@0.8.6';
 import * as grpc from 'npm:@grpc/grpc-js@1.12.5';
 import protobuf from 'npm:protobufjs@7.4.0';
+import { Buffer } from 'node:buffer';
 
 
 const NVIDIA_FUNCTION_ID = 'b75dbca7-b5a4-458c-9275-6d2effeb432a';
